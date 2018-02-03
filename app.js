429eaed1a8b4d5c917a63450e57aa5bf23287eef
plant99 <shivashispadhi@gmail.com>
2018-02-04 03:13:08 +0530

    Make compilebox compatible with new queue implementation and remove ANSI characters
    - make userId user_id as per the request
    - Remove ANSI characters from stdout before sending the response

--- app.js
+++ app.js
@@ -27,62 +27,68 @@ app.post('/dummt', function(req, res) {
 //dummy route
 app.post ('/compile', (req, res)=> {
 
-    let userId = req.body.userId;
+    let userId = req.body.user_id;
     let code = req.body.code;
     //console.log(userId);
 //    fs.rmdir('')
-    fs.stat('./compilebox_transaction', (err, stats) => {
-      if(err) {
-        //console.log(err);
-        return res.json({success: false});
-      }
-      if(stats.isDirectory()){
-        childProcess.exec('rm -rf ./compilebox_transaction && mkdir compilebox_transaction && mkdir compilebox_transaction/dlls && mkdir compilebox_transaction/source', (err, stdout, stderr) => {
-          //console.log(err, stdout, stderr, 'lpg');
-          fs.writeFile('./compilebox_transaction/source/player_code.cpp', code, (err) => {
-            if (err) throw err;
-            //prepare
-            childProcess.exec(
-              `
-                docker run -v $(pwd)/compilebox_transaction/dlls:/root/output_libs -v $(pwd)/compilebox_transaction/source:/root/codecharacter/src/player_code/src -t deltanitt/codecharacter-compiler:latest
-              `,
-              (error, stdout, stderr) => {
-                console.log(error, stderr, stdout);
-                if(req.body.secretString != secretKey){
-                    return res.json({code: 404, message:"Bad request!"});
-                }
-                //let respo
-                let dll1 = fs.readFileSync('./compilebox_transaction/dlls/libplayer_1_code.so');
-                let dll2 = fs.readFileSync('./compilebox_transaction/dlls/libplayer_2_code.so');
-                //console.log(dll1.length, dll1_decompressed.length);
-                if (error) {
-                  console.error(`exec error: ${error}`);
-                  return;
-                }
-                //console.log(`stdout: ${stdout}`);
-                //console.log(`stderr: ${stderr}`);
-                res.setHeader('scores', '0+56');
+    try{
+        fs.stat('./compilebox_transaction', (err, stats) => {
+          if(err) {
+            console.log(err);
+            //console.log(err);
+            return res.json({success: false});
+          }
+          if(stats.isDirectory()){
+            childProcess.exec('rm -rf ./compilebox_transaction && mkdir compilebox_transaction && mkdir compilebox_transaction/dlls && mkdir compilebox_transaction/source', (err, stdout, stderr) => {
+              //console.log(err, stdout, stderr, 'lpg');
+              fs.writeFile('./compilebox_transaction/source/player_code.cpp', code, (err) => {
+                if (err) throw err;
+                //prepare
+                childProcess.exec(
+                  `
+                    docker run -v $(pwd)/compilebox_transaction/dlls:/root/output_libs -v $(pwd)/compilebox_transaction/source:/root/codecharacter/src/player_code/src -t deltanitt/codecharacter-compiler:latest
+                  `,
+                  (error, stdout, stderr) => {
+                    console.log(error, stderr, stdout);
+                    if(req.body.secretString != secretKey){
+                        return res.json({code: 404, message:"Bad request!"});
+                    }
+                    //let respo
+                    let dll1 = fs.readFileSync('./compilebox_transaction/dlls/libplayer_1_code.so');
+                    let dll2 = fs.readFileSync('./compilebox_transaction/dlls/libplayer_2_code.so');
+                    //console.log(dll1.length, dll1_decompressed.length);
+                    if (error) {
+                      console.error(`exec error: ${error}`);
+                      return;
+                    }
+                    //console.log(`stdout: ${stdout}`);
+                    //console.log(`stderr: ${stderr}`);
+                    res.setHeader('scores', '0+56');
 
-                res.setHeader('user_id', userId);
-
-                if(stdout.toLowerCase().indexOf('error') != -1){
-                  return res.json({
-                    success: false,
-                    error: stdout
-                  });
-                }
-                return res.json({
-                  success: true,
-                  dll1,
-                  dll2
+                    res.setHeader('user_id', userId);
+                    stdout = stdout.replace(/[\u001b\u009b][[()#;?]*(?:[0-9]{1,4}(?:;[0-9]{0,4})*)?[0-9A-ORZcf-nqry=><]/g, '')
+                    console.log(stdout);
+                    if(stdout.toLowerCase().indexOf('error') != -1){
+                      return res.json({
+                        success: false,
+                        error: stdout
+                      });
+                    }
+                    return res.json({
+                      success: true,
+                      dll1,
+                      dll2
+                    });
                 });
             });
-        });
-        });
+            });
 
 
-      }
-    })
+          }
+        })
+    }catch(e){
+        console.log(e, 'Please make compilebox directory manually!');
+    }
 });
 app.post ('/execute', (req, res)=> {
     let matchId = req.body.matchId;
@@ -90,63 +96,67 @@ app.post ('/execute', (req, res)=> {
     let dll2 = new Buffer.from(req.body.dll2);
     //dll1 = Buffer.from(dll1, 'base64');
     //dll2 = Buffer.from(dll2, 'base64');
-    fs.stat('./executebox_transaction', (err, stats) => {
-        if(err) {
-            //console.log(err);
-            return res.json({success: false});
-        }
-        if(stats.isDirectory()){
-            childProcess.execSync('rm -rf ./executebox_transaction && mkdir executebox_transaction && mkdir executebox_transaction/dlls && mkdir executebox_transaction/output_log');
-            //fs.writeFileSync('./executebox_transaction/dlls/libplayer_1_code.so', dll1);
-            //fs.writeFileSync('./executebox_transaction/dlls/libplayer_2_code.so', dll2);
-            fs.writeFile(__dirname+'/executebox_transaction/dlls/libplayer_1_code.so', dll1, (err) => {
-                if (err) throw err;
-                fs.writeFile(__dirname+'/executebox_transaction/dlls/libplayer_2_code.so', dll2, (err) => {
-                    if(err) throw err;
-                    //prepare
-                    childProcess.exec(
-                            `
-                            docker run -v $(pwd)/executebox_transaction/dlls:/root/input_libs -v $(pwd)/executebox_transaction/output_log:/root/output_log -t deltanitt/codecharacter-runner:latest
-                            `,
-                            (error, stdout, stderr) => {
-                                let log = fs.readFileSync('executebox_transaction/output_log/game.log');
-                                let stdoutArray = stdout.split('\n');
-                                let results = stdoutArray[stdoutArray.length-2];
-                                //console.log(results);
+    try{
+        fs.stat('./executebox_transaction', (err, stats) => {
+            if(err) {
+                //console.log(err);
+                return res.json({success: false});
+            }
+            if(stats.isDirectory()){
+                childProcess.execSync('rm -rf ./executebox_transaction && mkdir executebox_transaction && mkdir executebox_transaction/dlls && mkdir executebox_transaction/output_log');
+                //fs.writeFileSync('./executebox_transaction/dlls/libplayer_1_code.so', dll1);
+                //fs.writeFileSync('./executebox_transaction/dlls/libplayer_2_code.so', dll2);
+                fs.writeFile(__dirname+'/executebox_transaction/dlls/libplayer_1_code.so', dll1, (err) => {
+                    if (err) throw err;
+                    fs.writeFile(__dirname+'/executebox_transaction/dlls/libplayer_2_code.so', dll2, (err) => {
+                        if(err) throw err;
+                        //prepare
+                        childProcess.exec(
+                                `
+                                docker run -v $(pwd)/executebox_transaction/dlls:/root/input_libs -v $(pwd)/executebox_transaction/output_log:/root/output_log -t deltanitt/codecharacter-runner:latest
+                                `,
+                                (error, stdout, stderr) => {
+                                    let log = fs.readFileSync('executebox_transaction/output_log/game.log');
+                                    let stdoutArray = stdout.split('\n');
+                                    let results = stdoutArray[stdoutArray.length-2];
+                                    //console.log(results);
 
-                                let player1Log = fs.readFileSync('./executebox_transaction/output_log/player_1.dlog');
-                                let player2Log = fs.readFileSync('./executebox_transaction/output_log/player_2.dlog');
-                                let player1LogCompressed = zlib.gzipSync(player1Log);
-                                let player2LogCompressed = zlib.gzipSync(player2Log);
-                                //let dll_compressed2 = zlib.gzipSync(dll2);
-                                //console.log(player1Log.length, player1LogCompressed.length, 'hey');
-                                //let dll1_decompressed = zlib.unzipSync(player1LogCompressed);
-                                //console.log(dll1_decompressed.length);
-                                //console.log(score1, score2);
-                                if(stdout.toLowerCase().indexOf('error') != -1){
-                                    return res.json({
-                                        success: false,
-                                        error: stdout,
-                                        matchId
-                                    });
-                                }
+                                    let player1Log = fs.readFileSync('./executebox_transaction/output_log/player_1.dlog');
+                                    let player2Log = fs.readFileSync('./executebox_transaction/output_log/player_2.dlog');
+                                    let player1LogCompressed = zlib.gzipSync(player1Log);
+                                    let player2LogCompressed = zlib.gzipSync(player2Log);
+                                    //let dll_compressed2 = zlib.gzipSync(dll2);
+                                    //console.log(player1Log.length, player1LogCompressed.length, 'hey');
+                                    //let dll1_decompressed = zlib.unzipSync(player1LogCompressed);
+                                    //console.log(dll1_decompressed.length);
+                                    //console.log(score1, score2);
+                                    if(stdout.toLowerCase().indexOf('error') != -1){
+                                        return res.json({
+                                            success: false,
+                                            error: stdout.replace(/[\u001b\u009b][[()#;?]*(?:[0-9]{1,4}(?:;[0-9]{0,4})*)?[0-9A-ORZcf-nqry=><]/g, ''),
+                                            matchId
+                                        });
+                                    }
 
-                                if (error || stderr) {
-                                    //console.log(error, stdout, stderr);
-                                    console.error(`exec error: ${error}`);
-                                    return res.json({
-                                        success: false,
-                                        error: stdout,
-                                        matchId
-                                    });
-                                }
+                                    if (error || stderr) {
+                                        //console.log(error, stdout, stderr);
+                                        console.error(`exec error: ${error}`);
+                                        return res.json({
+                                            success: false,
+                                            error: stdout.replace(/[\u001b\u009b][[()#;?]*(?:[0-9]{1,4}(?:;[0-9]{0,4})*)?[0-9A-ORZcf-nqry=><]/g, ''),
+                                            matchId
+                                        });
+                                    }
 
-                                res.json({success: true, log: log, matchId, results, player1LogCompressed, player2LogCompressed});
-                            });
-                })
-            });
-        }
-    })
+                                    res.json({success: true, log: log, matchId, results, player1LogCompressed, player2LogCompressed});
+                                });
+                    })
+                });
+            }
+        })
+    }catch(e){
+        console.log(e, "Please make executebox directory manually!");
+    }
 });
 
 /// catch 404 and forwarding to error handler
