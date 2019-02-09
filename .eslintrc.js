b4160ea843447382d60b11c704136ded73add66f
venkat24 <venkatramansrikanth@outlook.com>
2019-02-10 03:13:57 +0530

    Update ESLint conf

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,13 @@
+const OFF = 0, WARN = 1, ERROR = 2;
+
 module.exports = {
-    "extends": "airbnb-base"
+    "extends": "airbnb-base",
+    "rules" : {
+        "no-console": OFF,
+        "no-control-regex": OFF,
+        "new-cap": OFF,
+        "padded-blocks": OFF,
+        "consistent-return": OFF,
+        "radix": OFF,
+    }
 };
