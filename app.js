1f36d32026522b3a366eaaad243f1aa14ab1e5fe
venkat24 <venkatramansrikanth@outlook.com>
2019-02-10 22:30:30 +0530

    Refactor app.js and add map and key file in execute
    - Execute now takes map as a param, write to map.txt
    - Generates random key file and writes to key.txt
    - Refactored app.js with async-await as opposed to callbacks

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const bodyParser = require('body-parser');
 const childProcess = require('child_process');
 const zlib = require('zlib');
 const fs = require('fs');
+const util = require('util');
+const randomstring = require('randomstring');
 
 // Init Config Constants
 const secretKey = require('./config.js').configKey;
@@ -21,16 +23,45 @@ app.use(cookieParser());
 const COMPILE_DIRECTORY = 'compilebox_transaction';
 const EXECUTE_DIRECTORY = 'executebox_transaction';
 const PLAYER_CODE_DIRECTORY = '/root/codecharacter/src/player_code/src';
-const COMPILER_IMAGE = 'deltanitt/codecharacter-compiler:latest';
-const RUNNER_IMAGE = 'deltanitt/codecharacter-runner:latest';
+const COMPILER_IMAGE = 'deltanitt/codecharacter-compiler-2019:latest';
+const RUNNER_IMAGE = 'deltanitt/codecharacter-runner-2019:latest';
 
 // Helper function to strip ANSI characters from console output
 const stripAnsi = input => input.replace(/[\u001b\u009b][[()#;?]*(?:[0-9]{1,4}(?:;[0-9]{0,4})*)?[0-9A-ORZcf-nqry=><]/g, '');
 
+// Helper function to parse results from the result line output of the simulator
+const parseResultsFromString = (resultsString) => {
+  const resultsItems = resultsString.split(' ');
+
+  // Destructure results string
+  const [key, player1Score, player1Status, player2Score, player2Status] = resultsItems;
+
+  return {
+    key,
+    scores: [
+      {
+        score: parseInt(player1Score),
+        status: player1Status,
+      },
+      {
+        score: parseInt(player2Score),
+        status: player2Status,
+      },
+    ],
+  };
+};
+
+// Promisified async versions of standard functions
+const readFileAsync = util.promisify(fs.readFile);
+const writeFileAsync = util.promisify(fs.writeFile);
+const statFileAsync = util.promisify(fs.stat);
+const execChildProcessAsync = util.promisify(childProcess.exec);
+
 // Compile Route
-app.post('/compile', (req, res) => {
+app.post('/compile', async (req, res) => {
   const { code, secretString } = req.body;
 
+  // Check compilebox security key
   if (secretString !== secretKey) {
     return res.json({
       success: false,
@@ -39,192 +70,188 @@ app.post('/compile', (req, res) => {
   }
 
   try {
-    // Check if the container directory exists
-    fs.stat(`./${COMPILE_DIRECTORY}`, (directoryCheckError, stats) => {
-      if (directoryCheckError) {
-        console.log(directoryCheckError);
-        return res.json({
-          success: false,
-          error: directoryCheckError,
-        });
-      }
+    const directoryCheckStat = await statFileAsync(`./${COMPILE_DIRECTORY}`);
 
-      // If it does, check if it's a valid directory
-      if (stats.isDirectory()) {
-        // Remove anything previously contained in the directory and create new subdirectories
-        childProcess.exec(`
-            rm -rf ./${COMPILE_DIRECTORY} && \
-            mkdir -p ${COMPILE_DIRECTORY}/dlls && \
-            mkdir -p ${COMPILE_DIRECTORY}/source`,
-        (directoryCreationError) => {
-          if (directoryCreationError) {
-            console.log(directoryCreationError);
-            return res.json({
-              success: false,
-              error: directoryCreationError,
-            });
-          }
+    // If it's not a valid directory, quit
+    if (!directoryCheckStat.isDirectory()) {
+      console.log(`Please create the ${COMPILE_DIRECTORY} directory. Aborting...`);
+      return res.json({
+        success: false,
+        error: 'Internal Server Error',
+      });
+    }
 
-          // Write the player code into a file to ready it for compilation
-          fs.writeFile(`./${COMPILE_DIRECTORY}/source/player_code.cpp`, code, (playerCodeWriteError) => {
-            if (playerCodeWriteError) {
-              console.log(playerCodeWriteError);
-              return res.json({
-                success: false,
-                error: playerCodeWriteError,
-              });
-            }
+    // Remove anything previously contained in the directory and create new subdirectories
+    await execChildProcessAsync(`
+      rm -rf ./${COMPILE_DIRECTORY} && \
+      mkdir -p ${COMPILE_DIRECTORY}/dlls && \
+      mkdir -p ${COMPILE_DIRECTORY}/source`);
 
-            // Launch the compile container, passing the directories as params
-            // Once compile is done, the output libs are mapped back into the libs directory
-            childProcess.exec(`
-                    docker run \
-                    -v $(pwd)/${COMPILE_DIRECTORY}/dlls:/root/output_libs \
-                    -v $(pwd)/${COMPILE_DIRECTORY}/source:${PLAYER_CODE_DIRECTORY} \
-                    -t ${COMPILER_IMAGE}`,
-            (compilerError, stdout, stderr) => {
-              // Log outputs for visibility
-              console.log(stdout, stderr);
+    // Write the player code into a file to ready it for compilation
+    await writeFileAsync(`./${COMPILE_DIRECTORY}/source/player_code.cpp`, code);
 
-              // Handle compilation failure
-              if (compilerError) {
-                console.error(compilerError);
-                return compilerError;
-              }
+    // Launch the compile container, passing the directories as params
+    // Once compile is done, the output libs are mapped back into the libs directory
+    const { stdout, stderr } = await execChildProcessAsync(`
+      docker run \
+      -v $(pwd)/${COMPILE_DIRECTORY}/dlls:/root/output_libs \
+      -v $(pwd)/${COMPILE_DIRECTORY}/source:${PLAYER_CODE_DIRECTORY} \
+      -t ${COMPILER_IMAGE}`);
 
-              // If there was an error in compilation
-              if (stdout.toLowerCase().indexOf('error') !== -1) {
-                // Strip ANSI special color characters in compiler output
-                const strippedStdout = stripAnsi(stdout);
-                return res.json({
-                  success: false,
-                  error: strippedStdout,
-                });
-              }
+    // Log outputs for visibility
+    console.log(stdout, stderr);
 
-              // Read the output DLLs
-              const dll1 = fs.readFileSync(`./${COMPILE_DIRECTORY}/dlls/libplayer_1_code.so`);
-              const dll2 = fs.readFileSync(`./${COMPILE_DIRECTORY}/dlls/libplayer_2_code.so`);
+    // If there was an error in compilation
+    if (stdout.toLowerCase().indexOf('error:') !== -1) {
+      // Strip ANSI special color characters in compiler output
+      const strippedStdout = stripAnsi(stdout);
+      return res.json({
+        success: false,
+        error: strippedStdout,
+      });
+    }
 
-              // Return response
-              return res.json({
-                success: true,
-                dll1,
-                dll2,
-              });
-            });
-          });
-        });
-      }
+    // Read the output DLLs
+    const dll1 = await readFileAsync(`./${COMPILE_DIRECTORY}/dlls/libplayer_1_code.so`);
+    const dll2 = await readFileAsync(`./${COMPILE_DIRECTORY}/dlls/libplayer_2_code.so`);
+
+    // Return response
+    return res.json({
+      success: true,
+      dll1,
+      dll2,
     });
+
+  // Catch all
   } catch (e) {
-    console.log(e, 'Please make compilebox directory manually!');
+    console.error(e);
+    return e;
   }
 });
 
 // Execute Route
-app.post('/execute', (req, res) => {
-  const { matchId } = req.body;
+app.post('/execute', async (req, res) => {
+  const { map, secretString } = req.body;
   let { dll1, dll2 } = req.body;
+
+  // Check compilebox security key
+  if (secretString !== secretKey) {
+    return res.json({
+      success: false,
+      message: 'Unauthorized!',
+    });
+  }
 
   // Read DLL data into Buffer objects
   dll1 = new Buffer.from(req.body.dll1);
   dll2 = new Buffer.from(req.body.dll2);
 
+  // App security key
+  const key = randomstring.generate();
+
   try {
     // Check if the container directory exists
-    fs.stat(`./${EXECUTE_DIRECTORY}`, (directoryCheckError, stats) => {
-      if (directoryCheckError) {
-        return res.json({
-          success: false,
-          error: directoryCheckError,
-        });
-      }
+    const directoryCheckStat = await statFileAsync(`./${EXECUTE_DIRECTORY}`);
 
-      // Check if it's a valid directory
-      if (stats.isDirectory()) {
-        // Remove anything previously contained in the directory and create new subdirectories
-        childProcess.execSync(`
-          rm -rf ./${EXECUTE_DIRECTORY} && \
-          mkdir -p ${EXECUTE_DIRECTORY}/dlls && \
-          mkdir -p ${EXECUTE_DIRECTORY}/output_log`);
+    // If it's not a valid directory, quit
+    if (!directoryCheckStat.isDirectory()) {
+      console.log(`Please create the ${EXECUTE_DIRECTORY} directory. Aborting...`);
+      return res.json({
+        success: false,
+        error: 'Internal Server Error',
+      });
+    }
 
-        // Write input DLLs to file
-        fs.writeFile(`${__dirname}/${EXECUTE_DIRECTORY}/dlls/libplayer_1_code.so`, dll1, (dll1WriteError) => {
-          if (dll1WriteError) throw dll1WriteError;
-          fs.writeFile(`${__dirname}/${EXECUTE_DIRECTORY}/dlls/libplayer_2_code.so`, dll2, (dll2WriteError) => {
-            if (dll2WriteError) throw dll2WriteError;
+    // Remove anything previously contained in the directory and create new subdirectories
+    await execChildProcessAsync(`
+      rm -rf ./${EXECUTE_DIRECTORY} && \
+      mkdir -p ${EXECUTE_DIRECTORY}/dlls && \
+      mkdir -p ${EXECUTE_DIRECTORY}/output_log`);
 
-            // Launch the execute container, and pass output directories as parameters
-            childProcess.exec(`
-              docker run -v $(pwd)/${EXECUTE_DIRECTORY}/dlls:/root/input_libs \
-              -v $(pwd)/${EXECUTE_DIRECTORY}/output_log:/root/output_log \
-              -t ${RUNNER_IMAGE}`,
-            (executionError, stdout, stderr) => {
-              // Log output for visibility
-              console.log(executionError, stdout, stderr);
+    // Write the player DLLs
+    await writeFileAsync(`${__dirname}/${EXECUTE_DIRECTORY}/dlls/libplayer_1_code.so`, dll1);
+    await writeFileAsync(`${__dirname}/${EXECUTE_DIRECTORY}/dlls/libplayer_2_code.so`, dll2);
 
-              // If there was a visible error during runtime
-              if (stdout.toLowerCase().indexOf('error') !== -1) {
-                return res.json({
-                  success: false,
-                  error: stripAnsi(stdout),
-                  matchId,
-                });
-              }
+    // Write the map and key files
+    await writeFileAsync(`${__dirname}/${EXECUTE_DIRECTORY}/dlls/map.txt`, map);
+    await writeFileAsync(`${__dirname}/${EXECUTE_DIRECTORY}/dlls/key.txt`, key);
 
-              // If some other runtime error occured
-              if (executionError || stderr) {
-                console.error(executionError);
-                return res.json({
-                  success: false,
-                  error: stripAnsi(stdout),
-                  matchId,
-                });
-              }
+    // Launch the execute container, and pass output directories as parameters
+    const { stdout, stderr } = await execChildProcessAsync(`
+      docker run \
+      -v $(pwd)/${EXECUTE_DIRECTORY}/dlls:/root/input_libs \
+      -v $(pwd)/${EXECUTE_DIRECTORY}/output_log:/root/output_log \
+      -t ${RUNNER_IMAGE}`);
 
-              // Get the game scores from the end of stdout
-              const stdoutArray = stdout.split('\n');
-              const results = stdoutArray[stdoutArray.length - 2];
+    // Log output for visibility
+    console.log(stdout, stderr);
 
-              // If the game ended with an UNDEFINED status, return blank
-              if (results.indexOf('UNDEFINED') !== -1) {
-                return res.json({
-                  success: true,
-                  log: '',
-                  matchId,
-                  results,
-                  player1LogCompressed: '',
-                  player2LogCompressed: '',
-                });
-              }
+    // If there was a visible error during runtime
+    if (stdout.toLowerCase().indexOf('error') !== -1) {
+      return res.json({
+        success: false,
+        error: stripAnsi(stdout),
+      });
+    }
 
-              // Else, we write the game log to file, and compress
-              const log = fs.readFileSync(`${EXECUTE_DIRECTORY}/output_log/game.log`);
-              const logCompressed = zlib.gzipSync(log);
+    // If some other runtime error occured
+    if (stderr) {
+      return res.json({
+        success: false,
+        error: stripAnsi(stderr),
+      });
+    }
 
-              // Write player debug logs to file, and compress
-              const player1Log = fs.readFileSync(`./${EXECUTE_DIRECTORY}/output_log/player_1.dlog`);
-              const player2Log = fs.readFileSync(`./${EXECUTE_DIRECTORY}/output_log/player_2.dlog`);
-              const player1LogCompressed = zlib.gzipSync(player1Log);
-              const player2LogCompressed = zlib.gzipSync(player2Log);
+    // Get the game scores from the end of stdout
+    const stdoutArray = stdout.split('\n');
+    const resultsString = stdoutArray[stdoutArray.length - 2];
 
-              // Return response with logs and results
-              res.json({
-                success: true,
-                log: logCompressed,
-                matchId,
-                results,
-                player1LogCompressed,
-                player2LogCompressed,
-              });
-            });
-          });
-        });
-      }
+    const results = parseResultsFromString(resultsString);
+
+    // Ensure the security key matches
+    if (results.key !== key) {
+      return res.json({
+        success: false,
+        error: 'Security key mismatch! Possibly result tampering by player.',
+      });
+    }
+
+    // If we've gotten this far, the security keys match. Remove it from results
+    delete results.key;
+
+    // If the game ended with an UNDEFINED status, return blank
+    if (results.indexOf('UNDEFINED') !== -1) {
+      return res.json({
+        success: true,
+        log: '',
+        results,
+        player1LogCompressed: '',
+        player2LogCompressed: '',
+      });
+    }
+
+    // Else, we write the game log to file, and compress
+    const log = await readFileAsync(`${EXECUTE_DIRECTORY}/output_log/game.log`);
+    const logCompressed = zlib.gzipSync(log);
+
+    // Write player debug logs to file, and compress
+    const player1Log = await readFileAsync(`./${EXECUTE_DIRECTORY}/output_log/player_1.dlog`);
+    const player2Log = await readFileAsync(`./${EXECUTE_DIRECTORY}/output_log/player_2.dlog`);
+    const player1LogCompressed = zlib.gzipSync(player1Log);
+    const player2LogCompressed = zlib.gzipSync(player2Log);
+
+    // Return response with logs and results
+    res.json({
+      success: true,
+      log: logCompressed,
+      results,
+      player1LogCompressed,
+      player2LogCompressed,
     });
+
+  // Catch all
   } catch (e) {
-    console.error(e, 'Please create the executebox directory!');
+    console.error(e);
     return e;
   }
 });
